8dcdacc0c446f2ec756fb89f41ed93d72441c68f
jporobicg <jporobicg@gmail.com>
2017-01-15 16:23:45 +1100

    removing testing files Cleaning the code ToDo. Remove the garbage and add more functionality to the code       Compile and transform in library

--- model.R
+++ model.R
@@ -13,19 +13,19 @@ est   <- read.dat('data/jf_f.rep')
 ## Just as reminder the order of the zones are
 zones <- c('RC-SC NO', 'RC-SC NE', 'RC-SC SO', 'RC-SC SE', 'AS-NE', 'AS-SE', 'AS-SO', 'AS-NO')
 
-
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ##          Conectivity Matrix            ##
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
 ## Pelagic connectivity matrix
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#pela.mat <- read.dat('data/conect_mat.dat')[[3]]
-pela.mat <- diag(1, 1, 1) ## Matrix to test the model, with one population
+pela.mat <- read.dat('data/conect_mat.dat')[[2]]
+#pela.mat <- diag(1, 8) ## Matrix to test the model, with one population
 
 ## Benthic connectivity matrix
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#bent.mat <- read.dat('data/conect_mat.dat')[[5]]
-bent.mat <- diag(0, 1, 1) ## Matrix to test the model, with one population
+bent.mat <- read.dat('data/conect_mat.dat')[[5]]
+#bent.mat <- diag(0, 8) ## Matrix to test the model, with one population
 
 ##~~~~~~~~~~~~~~~~~~~~##
 ##  Known parameters  ##
@@ -54,94 +54,141 @@ stpnss.h <- 0.7
 ##     Estimated in ADMB                  ##
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
-R0         <- est$Rec[1] # / 4  # Virginal Recruitment
+R0         <- est$Rec[1] / 4            # Virginal Recruitment
 size.vec   <- with(datos, seq(from = lowersize, to = bigsize, by = sizedelta))
 res.Rec    <- log(est$Recruitment_Residuals)             # Recruitment residual
-res.Rec2   <- 0           # Recruitment residual
-#Rec        <- est$Rec
 Fs         <- with(param, exp(c(logF_1, logF_2, logF_3, logF_4)))
-
-
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ##      Equation and models               ##
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
-start.year <- 1901
-end.year   <- 2011
-
+start.year      <- 1901
+end.year        <- 2011
 fecundity       <- fec(a.fec, b.fec, size.vec)                                       # Fecundity
 maturity        <- matu(a.mat, b.mat, size.vec)                                      # Maturity
 w.l             <- alometry.f(a.wl, b.wl, size.vec)                                  # Alometric relationship
 f.selec         <- selectivity.f(f.l50, f.l95, size.vec)                             # Fishery Selectivity
 p.selec         <- selectivity.f(p.l50, p.l95, size.vec)                             # Trap Selectivity
-#p.selec <- est$Selectivity_pot
-
 normal.t.matrix <- trans.matrix(size.vec, k.g = 0.06985, linf = 213.495, beta.g = 1) # Normalize transition matrix
 mig.pat         <- c(rep(0.3, 28), rep(0.6, 8), rep(1, 67))                          # Migration Pattern (Puerulus - juvenil - adult)
 n.rec.pat       <- rec.pat(size.vec, beta = 0.3, alfa = 45)                          # Normalize recruitment pattern
-#h.effort <- c(0.84, 0.24, 2.32, 0.60, 0.72, 0.84, 1.08, 1.40) # Historical effort by zone
-h.effort <- c(1, 1, 1, 1, 1, 1, 1, 1) # Historical effort by zone
-
+h.effort        <- c(0.84, 0.24, 2.32, 0.6, 0.72, 0.8, 1.08, 1.4)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ##          Simulation                    ##
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
-n.zones   <- 1
+n.zones   <- 8
 years.sim <- start.year : end.year
-reclut    <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
-rec       <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
-rec2      <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
-rec3      <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
-b.catch   <- v.biomass <- spawners  <- rec
-## Array for the abundance and the biomass
-abundance2 <- abundance <- array(0, dim = c(length(zones), length(years.sim), length(size.vec)))
-aamig2    <- aamig <- catch <- alive <- abundance
-
-f.cur <- c(rep(Fs[1], 29), rep(Fs[2], 51), rep(Fs[3], 19), rep(Fs[4], 12))
+n.years   <- length(years.sim)
+## reclut    <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
+## rec       <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
+## rec2      <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
+## rec3      <- matrix(NA, ncol= length(years.sim), nrow = n.zones)
+## b.catch   <- v.biomass <- spawners  <- rec
+## ## Array for the abundance and the biomass
+## abundance <- array(0, dim = c(length(zones), length(years.sim), length(size.vec)))
+## aamig2    <- aamig <- catch <- alive <- abundance
+f.cur     <- c(rep(Fs[1], 29), rep(Fs[2], 51), rep(Fs[3], 19), rep(Fs[4], 12))
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~        At equilibrium    ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
-##~ initial recruitment
-rec[, 1]  <- R0
-rec2[, 1] <- R0
-rec3[, 1] <- R0
+output.simu <- simulation(M = M, R0 = R0, stpnss.h = stpnss.h, n.years = n.years, maturity = maturity, f.cur = f.cur, w.l = w.l,
+                          f.selec = f.selec, fecundity = fecundity, pela.mat = pela.mat, bent.mat = bent.mat, n.zones = 8,
+                          mig.pat = mig.pat, n.rec.pat = n.rec.pat, normal.t.matrix = normal.t.matrix, res.Rec = res.Rec,
+                          projection = FALSE)
 
+
+##-------------------
+## Projections
+##------------------
+summary(output.simu)
+pars <- output.simu$Parameters
+## Abundance of the last year of simulation
+abun.sim     <- output.simu$Abundance[1 : 8, 111, ]
+## Last f for the simulation
+f.end        <- f.cur[111]
+## last recruitment
+res.rec.end  <- res.Rec[111]
+## Years of projection
+n.years      <- 100
+seq.years    <- seq(from = 2012, length = n.years)
+## F-projection
+f.proj <- rep(f.end, n.years)
+## Residual deviations
+res.Rec.proj <- aut.rd(years = n.years, sigmaz = abs(mean(tail(res.Rec, 10))), phi = 0.95, seed = 800)
+## projection
+output.proj  <- simulation(M = M, R0 = R0, stpnss.h = stpnss.h, n.years = n.years, maturity = maturity, f.cur = f.proj, w.l = w.l,
+                          f.selec = f.selec, fecundity = fecundity, pela.mat = pela.mat, bent.mat = bent.mat, n.zones = 8,
+                          mig.pat = mig.pat, n.rec.pat = n.rec.pat, normal.t.matrix = normal.t.matrix, res.Rec = res.Rec.proj,
+                          projection = TRUE, t.rec.a = pars$t.rec.a, t.rec.b = pars$t.rec.b, abun.sim = abun.sim, f.end = f.end,
+                          res.rec.end = res.rec.end)
+
+
+
+
+
+## spgred   <- rgb(t(col2rgb("red")), alpha=10, maxColorValue=255)
+## plot(log(res.Rec  -  min(res.Rec)), type = 'l', ylim = c( - 4,  4))
+## for(i in 1 : 1000){
+##     res.Rec.proj <- aut.rd(years = n.years, sigmaz = abs(mean(tail(res.Rec, 10))), phi = 0.95, seed = i)
+##     lines(res.Rec.proj, col = spgred)
+## }
+
+
+## summary(output.proj)
+
+## output.proj$Abundance
+
+## Plots
+par(mar = c(1, 1, 1, 1), oma = c(1, 1, 1, 1), mfrow = c(2, 2))
+with(output.simu, plot(years.sim, colSums(B.catch[1 : 4 , ]), type = 'o', xlim = c(1900, 2060)))
+with(output.proj, lines(seq.years, colSums(B.catch[1 : 4, ]), col = 'blue'))
+
+with(output.simu, plot(years.sim, colSums(V.biomass[1 : 4, ]), type = 'o', xlim = c(1900, 2060)))
+with(output.proj, lines(seq.years, colSums(V.biomass[1 : 4, ]), col = 'blue'))
+
+with(output.simu, plot(years.sim, log(colSums(Rec[1:4, ])), type = 'o', xlim = c(1900, 2060)))
+with(output.proj, lines(seq.years, log(colSums(Rec[1 : 4, ])), col = 4))
+
+with(output.simu, plot(years.sim, log(colSums(Spawners[1 : 4 , ])), type = 'o', xlim = c(1900, 2060)))
+with(output.proj, lines(seq.years, log(colSums(Spawners[1 : 4, ])), col = 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##~ initial recruitment
+rec[, 1]      <- R0
 rec.shape     <- rec.shp(rec[, 1], pela.mat)
 for(z in 1 : n.zones){ # loop for the zones
     ##~ Abundance
     abundance[z, 1, ] <- (n.rec.pat %*% solve(diag(1, dim(normal.t.matrix)) - normal.t.matrix * exp(-M))) * rec.shape[z]
-    abundance2[z, 1, ] <- (n.rec.pat %*% solve(diag(1, dim(normal.t.matrix)) - normal.t.matrix * exp(-M))) * rec.shape[z]
-    #abundance[z, 1, ] <- est$Number[1,]
-    #abundance2[z, 1, ] <- est$Number[1,]
     ##~ Biomass
     v.biomass[z, 1] <- sum(abundance[z, 1, ] *  w.l * f.selec) / 1000000
 }
 ##~ Spawning biomass at equilibrium
-S0       <- spawn(fecundity, maturity, abundance[1, 1, ])#  / 4
-## S0M       <- spawn(fecundity, est$Maturity, sum(abundance[1 : 4, 1, ]))#  / 4
-## S0F       <- spawn(est$Fecundity, maturity, sum(abundance[1 : 4, 1, ]))#  / 4
-## S0N       <- spawn(fecundity, maturity, sum(est$N[1, ]))#  / 4
-## SOA <- spawn(est$Fecundity, est$Maturity, sum(est$N[1, ]))#  / 4
-
-## sum(abundance[1, 1, ] * fecundity * maturity)
-## sum(abundance[1, 1, ] * est$Reprod)
-
-## plot(aundance(fecundity * maturity)))
-
-## S0
-## S0M
-## S0F
-## S0N
-## SOA
-
-
-#S0      <- est$Spawners[1]
-t.rec.a <-  (S0 / R0)  * (1 - (stpnss.h - 0.2) / (0.8 * stpnss.h))
-t.rec.b <-  (stpnss.h - 0.2) / (0.8 * stpnss.h * R0)
-spawners[z, 1] <- S0 / n.zones
+S0            <- spawn(fecundity, maturity, colSums(abundance[1 : 4, 1, ])) / 4
+t.rec.a       <-  (S0 / R0 )  * (1 - (stpnss.h - 0.2) / (0.8 * stpnss.h))
+t.rec.b       <-  (stpnss.h - 0.2) / (0.8 * stpnss.h * R0)
+spawners[, 1] <- S0
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~           population with catch        ~ ##
@@ -151,7 +198,7 @@ for(year in 2 : length(years.sim)){ # years with fishery
     rec[, year]  <-  recruitment(t.rec.a, t.rec.b, spawners[, (year - 1)], exp(res.Rec[year]))
     ##~ Recruitment by zone after connectivity
     rec.shape  <- rec.shp(rec[, year], pela.mat)
-    for(z in n.zones){
+    for(z in 1 : n.zones){
         ## Abundance after Benthic migration
         aamig[z, year, ]     <- benth.mig(abundance[, year-1, ], bent.mat, mig.pat, z)
         ## Alive after catch, Natural moratality and migration
@@ -161,7 +208,7 @@ for(year in 2 : length(years.sim)){ # years with fishery
         ## Catch
         catch[z, year, ]     <- catch.f(abundance[, year, ], f.selec, f.cur[year], h.effort, M, z)
         ## Biomass catch
-        b.catch[z, year]     <- sum(catch[z, year, ] * w.l) / 1000000
+        B.catch[z, year]     <- sum(catch[z, year, ] * w.l) / 1000000
         ## Spawners
         spawners[z, year]    <- spawn(fecundity, maturity, abundance[z, year, ])
         ##~ Biomass
@@ -172,52 +219,55 @@ for(year in 2 : length(years.sim)){ # years with fishery
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ##         Projections 50 years            ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
-
 year.proj      <- 50
 seq.years      <- seq(from=2012, length=50)
 rec.proj       <- matrix(NA, ncol= year.proj, nrow = n.zones)
-b.catch.proj   <- v.biomass.proj <- spawners.proj  <- rec.proj
+B.catch.proj   <- v.biomass.proj <- spawners.proj  <- rec.proj
 ## Array for the abundance and the biomass
 abundance.proj <- array(0, dim = c(length(zones), year.proj, length(size.vec)))
 aamig.proj     <- catch.proj <- alive.proj <- abundance.proj
 ## Autocorrelation recruitment
 res.Rec.proj  <- aut.rd(years = 50, sigmaz = abs(mean(tail(res.Rec, 10))), phi = 0.95, seed = 800)
-cat(t(res.Rec.proj))
-plot(exp(res.Rec), type = 'l')
-lines(exp(res.Rec.proj), col = 2, lty = 2)
+## Fishing mortality
+f.proj <- rep(tail(f.cur, 1), year.proj)
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~             Projection                       ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ##~ First Year
-rec.proj[, 1] <- recruitment(t.rec.a, t.rec.b, spawners[, length(years.sim)], exp(res.Rec[length(years.sim)]))
+rec[, 1] <- recruitment(t.rec.a, t.rec.b, spawners[, length(years.sim)], exp(res.Rec[length(years.sim)]))
 rec.shape     <- rec.shp(rec.proj[, 1], pela.mat)
 
 for(z in 1 : n.zones){ # loop for the zones
-        abundance.proj[z, 1, ] <- abund(alive[z, length(years.sim), ], normal.t.matrix, n.rec.pat, rec.shape[z],)
+        abundance.proj[z, 1, ] <- abund(alive[z, length(years.sim), ], normal.t.matrix, n.rec.pat, rec.shape[z])
         v.biomass.proj[z, 1]   <- sum(abundance.proj[z, 1, ] *  w.l * f.selec) / 1000000
+        catch.proj[z, 1, ]     <- catch.f(abundance.proj[, 1, ], f.selec, f.proj[1], h.effort, M, z)
+        B.catch.proj[z, 1]     <- sum(catch.proj[z, 1, ] * w.l) / 1000000
         spawners.proj[z, 1]    <- spawn(fecundity, maturity, abundance.proj[z, 1, ])
 }
+
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~           population with catch        ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
 for(proj in 2 : year.proj){ # years with fishery
     ##~ Recruitment
     rec.proj[, proj]  <- recruitment(t.rec.a, t.rec.b, spawners.proj[, (proj - 1)], exp(res.Rec.proj[proj]))
     ##~ Recruitment by zone after connectivity
     rec.shape        <- rec.shp(rec.proj[, proj], pela.mat)
-        for(z in n.zones){
+
+        for(z in 1 : n.zones){
         ## Abundance after Benthic migration
-        aamig.proj[z, proj, ]     <- benth.mig(abundance[, proj-1, ], bent.mat, mig.pat, z)
+        aamig.proj[z, proj, ]     <- benth.mig(abundance[, proj - 1, ], bent.mat, mig.pat, z)
         ## Alive after catch, Natural moratality and migration
-        alive.proj[z, proj, ]     <- alive.f(aamig[, proj, ], M, f.selec, f.cur[proj - 1], h.effort, z)
+        alive.proj[z, proj, ]     <- alive.f(aamig[, proj, ], M, f.selec, f.proj[proj - 1], h.effort, z)
         ## Abundance
         abundance.proj[z, proj, ] <- abund(alive.proj[z, proj, ], normal.t.matrix, n.rec.pat, rec.shape[z])
         ## Catch
-        catch.proj[z, proj, ]     <- catch.f(abundance[, proj, ], f.selec, f.cur[proj], h.effort, M, z)
+        catch.proj[z, proj, ]     <- catch.f(abundance.proj[, proj, ], f.selec, f.proj[proj], h.effort, M, z)
         ## Biomass catch
-        b.catch.proj[z, proj]     <- sum(catch.proj[z, proj, ] * w.l) / 1000000
+        B.catch.proj[z, proj]     <- sum(catch.proj[z, proj, ] * w.l) / 1000000
         ## Spawners
         spawners.proj[z, proj]    <- spawn(fecundity, maturity, abundance.proj[z, proj, ])
         ##~ Biomass
@@ -227,19 +277,46 @@ for(proj in 2 : year.proj){ # years with fishery
 
 
 
+x11()
 par(mar = c(1, 1, 1, 1), oma = c(1, 1, 1, 1), mfrow = c(2, 2))
-plot(years.sim, b.catch[1 , ], type = 'o', xlim = c(1900, 2060))
+plot(years.sim, colSums(B.catch[1 : 4 , ]), type = 'o', xlim = c(1900, 2060))
 lines(years.sim, est$Catch, col = 2)
-lines(seq.years, b.catch.proj[, ], col = 'blue')
-plot(years.sim, v.biomass[1, ], type = 'o', xlim = c(1900, 2060))
+lines(seq.years, colSums(B.catch.proj[1 : 4, ]), col = 'blue')
+##lines(seq.years, B.catch.proj[1, ], col = 'blue')
+plot(years.sim, colSums(v.biomass[1 : 4, ]), type = 'o', xlim = c(1900, 2060))
 lines(years.sim, est$Biomass[-112], col = 2)
-lines(seq.years, v.biomass.proj[1, ], col = 'blue')
+lines(seq.years, colSums(v.biomass.proj[1 : 4, ]), col = 'blue')
 
-plot(years.sim, log(rec[1, ]), type = 'o', xlim = c(1900, 2060))
-rec.est <- log(est$Rec * c(1,exp(est$Recruitment_Residuals)[-111]))
+plot(years.sim, log(colSums(rec[1:4, ])), type = 'o', xlim = c(1900, 2060))
+rec.est <- log(est$Rec * c(1, exp(est$Recruitment_Residuals)[-111]))
 lines(years.sim, rec.est[-112], col = 2)
-lines(seq.years, log(rec.proj))
+lines(seq.years, log(colSums(rec.proj[1 : 4, ])), col = 4)
 
-plot(years.sim, log(spawners[1 , ]), type = 'o', xlim = c(1900, 2060))
+plot(years.sim, log(colSums(spawners[1 : 4 , ])), type = 'o', xlim = c(1900, 2060))
 lines(years.sim, log(est$Spawners[-112]), col = 2)
-lines(seq.years, log(spawners.proj))
+lines(seq.years, log(colSums(spawners.proj[1 : 4, ])), col = 4)
+
+
+
+
+
+
+
+
+n.years=length(years.sim)
+abun.sim <- abundance[1 : 8, 111, ]
+
+
+
+f.end <- f.cur[111]
+res.rec.end <- res.Rec[111]
+projection=T
+n.years=50
+
+
+
+salida <- simulation(M = M, R0 = R0, stpnss.h = stpnss.h, n.years = n.years, maturity = maturity, w.l = w.l,
+           f.selec = f.selec, fecundity = fecundity, pela.mat = pela.mat, bent.mat = bent.mat, n.zones = 8,
+           n.rec.pat = n.rec.pat, normal.t.matrix = normal.t.matrix, res.Rec = res.Rec,
+           projection = FALSE, t.rec.a = NULL, t.rec.b = NULL, abun.sim = NULL, f.end = NULL,
+           res.rec.end = NULL)
--- simulation.R
+++ simulation.R
@@ -1,18 +1,18 @@
-#==============================================================#
-#              MPA smulation Juan fernandez                    #
-# Creator  :  Javier Porobic                                   #
-# Date     :  6-Aug-2015                                       #
-# Last mod :  6-Aug-2015                                       #
-#==============================================================#
+##==============================================================#
+##              MPA smulation Juan fernandez                    #
+## Creator  :  Javier Porobic                                   #
+## Date     :  6-Aug-2015                                       #
+## Last mod :  6-Aug-2015                                       #
+##==============================================================#
 
 trans.matrix <- function(size.vec, k.g = NULL, linf = NULL, beta.g = NULL){
-    ##################################################################################
+##################################################################################
     ## Normalize Gamma shaped Transitin matrix                                      ##
     ## size.vec:   vector of legths
     ## beta.g  :   Beta from the gamma distribution
     ## k.g     :   Kappa from the gamma disgribution
     ## linf    :  maximum length in the polulation
-    ##################################################################################
+##################################################################################
     n.sizes <- length(size.vec)
     intervals <- (size.vec + diff(size.vec/2))
     intervals[length(intervals)] <- size.vec[length(size.vec)] # limits
@@ -32,31 +32,31 @@ trans.matrix <- function(size.vec, k.g = NULL, linf = NULL, beta.g = NULL){
 }
 
 read.dat <- function(file){
-        options(warn=-1)
-        #-----------------------------------------------------------#
-        #  Function to read data file in a ".dat" files             #
-        #        Creador:  Steve Martell  Mod: Javier Porobic       #
-        #-----------------------------------------------------------#
-        ifile <- scan(file, what = "character", flush = TRUE, blank.lines.skip = FALSE, quiet = TRUE)
-        idx   <- sapply(as.double(ifile), is.na)
-        vnam  <- ifile[idx]	                    #list names
-        nv    <- length(vnam)	                    #number of objects
-        A     <- list()
-        ir    <- 0
-        for(i in 1 : nv)
-            {
-                ir <- match(vnam[i], ifile)
-                if(i != nv) irr <- match(vnam[i + 1], ifile) else irr = length(ifile) + 1 #next row
-                dum <- NA
-                if(irr-ir==2) dum <- as.double(scan(file, skip = ir, nlines = 1, quiet = TRUE, what = ""))
-                if(irr-ir>2)  dum <- as.matrix(read.table(file, skip = ir, nrow = irr-ir-1, fill = TRUE))
-                if(is.numeric(dum))#Logical test to ensure dealing with numbers
-                    {
-                        A[[ vnam[i ]]] <- dum
-                    }
-            }
-        return(A)
+    options(warn=-1)
+                                        #-----------------------------------------------------------#
+                                        #  Function to read data file in a ".dat" files             #
+                                        #        Creador:  Steve Martell  Mod: Javier Porobic       #
+                                        #-----------------------------------------------------------#
+    ifile <- scan(file, what = "character", flush = TRUE, blank.lines.skip = FALSE, quiet = TRUE)
+    idx   <- sapply(as.double(ifile), is.na)
+    vnam  <- ifile[idx]	                    #list names
+    nv    <- length(vnam)	                    #number of objects
+    A     <- list()
+    ir    <- 0
+    for(i in 1 : nv)
+    {
+        ir <- match(vnam[i], ifile)
+        if(i != nv) irr <- match(vnam[i + 1], ifile) else irr = length(ifile) + 1 #next row
+        dum <- NA
+        if(irr-ir==2) dum <- as.double(scan(file, skip = ir, nlines = 1, quiet = TRUE, what = ""))
+        if(irr-ir>2)  dum <- as.matrix(read.table(file, skip = ir, nrow = irr-ir-1, fill = TRUE))
+        if(is.numeric(dum))#Logical test to ensure dealing with numbers
+        {
+            A[[ vnam[i ]]] <- dum
+        }
     }
+    return(A)
+}
 
 fec <- function(a, b, size){
     ## Calculation of the fecundity at age
@@ -79,7 +79,7 @@ matu <- function(a, b, size){
 }
 
 alometry.f <- function(a, b, size){
-## Alometric function
+    ## Alometric function
     vect <- vector()
     size <- c(size, tail(size,1) + diff(size)[1])
     for( i in 1 : (length(size) - 1)){
@@ -144,7 +144,7 @@ benth.mig <- function(abundance, connect.mat, m.pattern, zone){
     inmig <- (colSums(t(t(abundance * connect.mat[, zone]) * mig.pat)))
     ## Migrants
     mig   <- (colSums(t(t(abundance[rep(zone, 8),  ] * connect.mat[zone,]) * mig.pat)))
-    #Total migration
+                                        #Total migration
     migration <- abundance[zone, ] + inmig - mig
     return(migration)
 }
@@ -163,11 +163,94 @@ alive.f <- function(abun, M, Sel.F, F, effort, zone){
     return(out)
 }
 
+
+simulation <- function(M, R0, stpnss.h, n.years, maturity, w.l, f.selec, f.cur, projection = FALSE, fecundity, pela.mat, bent.mat, n.zones, mig.pat,
+                       n.rec.pat, normal.t.matrix, res.Rec, t.rec.a = NULL, t.rec.b = NULL, abun.sim = NULL, f.end = NULL, res.rec.end=NULL){
+    abundance <- array(0, dim = c(n.zones, n.years, length(fecundity)))
+    aamig     <- catch <- alive <- abundance
+    rec       <- matrix(NA, ncol= n.years, nrow = n.zones)
+    b.catch   <- v.biomass <- spawners <- rec
+    if(!isTRUE(projection)){
+        ## |||||~~~~ In equilibrum ~~~~||||
+        ## ~ initial recruitment
+        rec[, 1]      <- R0
+        rec.shape     <- rec.shp(rec[, 1], pela.mat)
+        for(z in 1 : n.zones){ # loop for the zones
+            ##~ Abundance
+            abundance[z, 1, ] <- (n.rec.pat %*% solve(diag(1, dim(normal.t.matrix)) - normal.t.matrix * exp(-M))) * rec.shape[z]
+            ##~ Biomass
+            v.biomass[z, 1] <- sum(abundance[z, 1, ] *  w.l * f.selec) / 1000000
+        }
+        ##~ Spawning biomass at equilibrium
+        S0            <- spawn(fecundity, maturity, colSums(abundance[1 : 4, 1, ])) / 4
+        t.rec.a       <-  (S0 / R0 )  * (1 - (stpnss.h - 0.2) / (0.8 * stpnss.h))
+        t.rec.b       <-  (stpnss.h - 0.2) / (0.8 * stpnss.h * R0)
+        spawners[, 1] <- S0
+    } else {
+        if(any(c(is.null(t.rec.a), is.null(t.rec.b)))){
+            cat('\n\tfor projection you need to provide the values for alfa and beta in the spawning equation\n')
+        }
+       if(any(c(is.null(alive), is.null(abun.sim)))){
+           cat('\n\t You need to provide the initial abundance')
+       }
+        abundance[1 : n.zones, 1, ] <- abun.sim
+        for(z in 1 : n.zones){ # loop for the zones
+            v.biomass[z, 1]   <- sum(abundance[z, 1, ] *  w.l * f.selec) / 1000000
+            catch[z, 1, ]     <- catch.f(abundance[, 1, ], f.selec, f.end, h.effort, M, z)
+            b.catch[z, 1]     <- sum(catch[z, 1, ] * w.l) / 1000000
+            spawners[z, 1]    <- spawn(fecundity, maturity, abundance[z, 1, ])
+        }
+        rec[, 1]    <- recruitment(t.rec.a, t.rec.b, spawners[, 1], exp(res.rec.end))
+        rec.shape   <- rec.shp(rec[, 1], pela.mat)
+    }
+    for(year in 2 : n.years){
+        ##~ Recruitment
+        rec[, year]  <-  recruitment(t.rec.a, t.rec.b, spawners[, (year - 1)], exp(res.Rec[year]))
+        ##~ Recruitment by zone after connectivity
+        rec.shape  <- rec.shp(rec[, year], pela.mat)
+        for(z in 1 : n.zones){
+            ## Abundance after Benthic migration
+            aamig[z, year, ]     <- benth.mig(abundance[, year-1, ], bent.mat, mig.pat, z)
+            ## Alive after catch, Natural moratality and migration
+            alive[z, year, ]     <- alive.f(aamig[, year, ], M, f.selec, f.cur[year - 1], h.effort, z)
+            ## Abundance
+            abundance[z, year, ] <- abund(alive[z, year, ], normal.t.matrix, n.rec.pat, rec.shape[z])
+            ## Catch
+            catch[z, year, ]     <- catch.f(abundance[, year, ], f.selec, f.cur[year], h.effort, M, z)
+            ## Biomass catch
+            b.catch[z, year]     <- sum(catch[z, year, ] * w.l) / 1000000
+            ## Spawners
+            spawners[z, year]    <- spawn(fecundity, maturity, abundance[z, year, ])
+            ##~ Biomass
+            v.biomass[z, year]   <- sum(abundance[z, year, ] *  w.l * f.selec) / 1000000
+        }
+    }
+    output <- list(Abundance  = abundance,
+                   Catch      = catch,
+                   B.catch    = b.catch,
+                   Spawners   = spawners,
+                   V.biomass  = v.biomass,
+                   Rec        = rec)
+    if(!isTRUE(projection)){
+        output$Parameters <- list(S0 = S0, t.rec.a = t.rec.a, t.rec.b = t.rec.b)
+    }
+                   ## Parameters = ifelse(!isTRUE(projection), list(S0      = S0,
+                   ##                                               t.rec.a = t.rec.a,
+                   ##                                               t.rec.b = t.rec.b), NA))
+
+    return(output)
+}
+
+
+
+
+
+
 aut.rd <- function(years, sigmaz, phi, seed = NULL){
     if(!is.null(seed)) set.seed(seed)
-    rd     <- rnorm(50, sd = 3)
+    rd     <- rnorm(years, sd = 3)
     sigmae <- sigmaz * (1 - phi ^ 2)
-    zt     <- vector(mode = 'numeric',  length = 50)
+    zt     <- vector(mode = 'numeric',  length = years)
     zt[1]  <- sqrt(sigmaz) * rd[1]
     for(dt in 2 : years){
         zt[dt] <- zt[dt - 1] * phi + rd[dt] * sqrt(sigmae)
